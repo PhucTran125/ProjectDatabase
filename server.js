9ee445952b93b321be0107d4959e0ca855975332
PhucTran125 <phuctran125.hust@gmail.com>
2021-04-03 00:36:02 +0700

    new

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+
+// Static files
+app.use(express.static(__dirname + '/public'))
+// app.use('/Stylesheets', express.static(__dirname + 'public/Stylesheets'))
+
+
+// Set view
 app.set('view engine', 'ejs')
+
 
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('newindex')
 })
 
+// Listen to port 3000
 app.listen(3000)
