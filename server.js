af22c68c89e07646bc729c1a6de119b9c6084fcd
PhucTran125 <phuctran125.hust@gmail.com>
2021-04-21 11:20:56 +0700

    Escape

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const articleRouter = require('./routes/articles')
+// const catalogRouter = require('/routes/catalog')
 const app = express()
 
 
 // Static files
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/css'))
 // app.use('/Stylesheets', express.static(__dirname + 'public/Stylesheets'))
 
 
 // Set view
 app.set('view engine', 'ejs')
 
-
 app.use('/articles', articleRouter)
+// app.use('/catalog', catalogRouter)
 
 app.get('/', (req, res) => {
     res.render('newindex')
