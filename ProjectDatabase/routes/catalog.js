0cca4caeeaf2fc16f4a7bfd96202dc66e0d04b5e
PhucTran125 <phuctran125.hust@gmail.com>
2021-05-01 18:08:27 +0700

    1/5 final

--- ProjectDatabase/routes/catalog.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const catalogController = require('../app/controllers/CatalogController');
-
-// catalogController.index
-router.get('/:slug', catalogController.show);
-router.get('/', catalogController.index);
-
-module.exports = router;
