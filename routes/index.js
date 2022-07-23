272aebb23b6b44ca0af597a5bf8be71f41a1da21
krisspham <hpham2687@gmail.com>
2022-07-24 00:25:40 +0700

    feat: Handle pay with momo

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
-const catalogRouter = require('./catalog');
-const siteRouter = require('./site');
-const apiRouter = require('./db');
-const logRouter = require('./account');
-const cartRouter = require('./cart');
-const adminRouter = require('./admin-page');
+const catalogRouter = require("./catalog");
+const siteRouter = require("./site");
+const apiRouter = require("./db");
+const logRouter = require("./account");
+const cartRouter = require("./cart");
+const adminRouter = require("./admin-page");
+const momoRouter = require("./momo");
 
 function route(app) {
+  app.use("/admin-page", adminRouter);
 
-    
-    app.use('/admin-page', adminRouter)
+  app.use("/my-cart", cartRouter);
 
-    app.use('/my-cart', cartRouter);
-    
-    app.use('/account', logRouter);
+  app.use("/account", logRouter);
 
-    app.use('/api/test', apiRouter);
+  app.use("/api/test", apiRouter);
 
-    app.use('/catalog', catalogRouter);
+  app.use("/catalog", catalogRouter);
 
-    app.use('/', siteRouter);
-    
+  app.use("/momo", momoRouter);
+
+  app.use("/", siteRouter);
 }
 
 module.exports = route;
